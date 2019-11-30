157828b5070f177057248562ba6350b7fdb8faff
Bolshagin Vyacheslav <gbublik@gmail.com>
2019-11-30 18:22:45 +0300

    Update .description.php

--- .description.php
+++ .description.php
@@ -1,14 +1,13 @@
 <?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 
-$arComponentDescription = array(
-    "NAME" => "Список контактов в оргструктуре",
-    "DESCRIPTION" => "Компонент выводит список контактов с подгрузкой дочерних подразделений",
-    "ICON" => "/images/catalog.gif",
-    "SORT" => 10,
-    "PATH" => array(
-        "ID" => "Prodvigaeff",
-        "NAME" => "Prodvigaeff.ru",
-    ),
-);
-?>
+$arComponentDescription = [
+    'NAME' => 'Список залов',
+    'DESCRIPTION' => 'Список залов',
+    'ICON' => '/images/catalog.gif',
+    'SORT' => 10,
+    'PATH' => [
+        'ID' => 'Prodvigaeff',
+        'NAME' => 'Prodvigaeff.ru',
+    ],
+];
