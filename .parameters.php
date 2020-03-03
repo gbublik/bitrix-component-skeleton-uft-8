8ce383b1a2d5c2ac5d94d234cdde7edc4f678c67
Bolshagin Vyacheslav <gbublik@gmail.com>
2020-03-03 13:17:57 +0300

    select iblock exemple

--- .parameters.php
+++ .parameters.php
@@ -4,10 +4,31 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 
 if(!CModule::IncludeModule('iblock'))
     return;
+$arTypesEx = CIBlockParameters::GetIBlockTypes();
+
+$arIBlocks = Array();
+$db_iblock = CIBlock::GetList(Array("SORT"=>"ASC"), Array("SITE_ID"=>$_REQUEST["site"], "TYPE" => ($arCurrentValues["IBLOCK_TYPE"]!="-"?$arCurrentValues["IBLOCK_TYPE"]:"")));
+while($arRes = $db_iblock->Fetch())
+    $arIBlocks[$arRes["ID"]] = "[".$arRes["ID"]."] ".$arRes["NAME"];
 
 $arComponentParameters = [
     'GROUPS' => [],
     'PARAMETERS' => [
         'CACHE_TIME'  =>  ['DEFAULT' => 86400],
+        "IBLOCK_TYPE"  =>  Array(
+            "PARENT" => "BASE",
+            "NAME" => "Тип информационного блока",
+            "TYPE" => "LIST",
+            "VALUES" => $arTypesEx,
+            "DEFAULT" => "news",
+            "REFRESH" => "Y",
+        ),
+        "IBLOCK_ID"  =>  Array(
+            "PARENT" => "BASE",
+            "NAME" => "ID информационного блока",
+            "TYPE" => "LIST",
+            "VALUES" => $arIBlocks,
+            "DEFAULT" => '',
+        ),
     ]
 ];
